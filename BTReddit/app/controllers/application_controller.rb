9c9fb9cbcf871cfead19b9097a7e08468731099b
TheoObbard <theo@dali.dartmouth.edu>
2018-11-20 11:03:09 -0800

    Set up user model and applicationController methods

---
+++ BTReddit/app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def require_login
+    # come back here
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+end
